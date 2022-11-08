9ea7decf9e3da847c213782d5dffa1742e3762a6
ivanmanzi5 <ivanlae@outlook.com>
2022-11-08 14:05:00 +0200

    initial commit

--- user_manager_service/src/main/java/com/user_manager_service/UserManagerServiceApplication.java
+++ user_manager_service/src/main/java/com/user_manager_service/UserManagerServiceApplication.java
@@ -3,6 +3,8 @@ package com.user_manager_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UserManagerServiceApplication {
