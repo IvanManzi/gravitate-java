4c9a53582a98e9a0173f35f4ec81e9e449f70c9b
ivanmanzi5 <ivanlae@outlook.com>
2022-11-08 14:03:25 +0200

    added controller for services

--- user_manager_service/src/main/java/com/user_manager_service/controller/AuthenticationController.java
+++ user_manager_service/src/main/java/com/user_manager_service/controller/AuthenticationController.java
@@ -1,4 +1,65 @@
 package com.user_manager_service.controller;
 
+import com.util.APIResponse;
+import com.user_manager_service.form.LoginRequest;
+import com.util.JwtUtils;
+import com.util.UserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("api/v1/user")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) throws ParseException {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = JwtUtils.generateJwtToken(authentication);
+
+        UserDetailsService userDetails = (UserDetailsService) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        Map<String,Object> data = new HashMap<>();
+        data.put("USER",userDetails);
+        data.put("ROLES",roles);
+        data.put("JWT_TOKEN",jwt);
+
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .statusCode(200)
+                        .data(data)
+                        .build()
+        );
+    }
 }
--- user_manager_service/src/main/java/com/user_manager_service/controller/GravitateUserManagerController.java
+++ user_manager_service/src/main/java/com/user_manager_service/controller/GravitateUserManagerController.java
@@ -1,9 +1,100 @@
 package com.user_manager_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.util.APIResponse;
+import com.model.UserManagerVO;
+import com.model.UserVO;
+import com.user_manager_service.form.CreateGravitateUserForm;
+import com.user_manager_service.service.GravitateUserManagerService;
+import com.util.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RestController
 @RequestMapping(value = "api/v1/user", produces = "application/json")
+@RequiredArgsConstructor
 public class GravitateUserManagerController {
+
+    private final GravitateUserManagerService gravitateUserManagerService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<APIResponse> createGravitateUser(@RequestBody CreateGravitateUserForm createGravitateUserForm){
+        UserVO userVO = new UserVO();
+        UserManagerVO userManagerVO = new UserManagerVO();
+        //create user obj
+        userVO.setUserType(createGravitateUserForm.userType());
+        userVO.setEmail(createGravitateUserForm.email());
+        userVO.setPassword(passwordEncoder.encode(createGravitateUserForm.password()));
+        userVO.setAlternateEmail(createGravitateUserForm.alternativeEmail());
+        userVO.setFirstName(createGravitateUserForm.firstName());
+        userVO.setLastName(createGravitateUserForm.lastName());
+        userVO.setCountry(createGravitateUserForm.country());
+        userVO.setDateOfBirth(createGravitateUserForm.dob());
+        userVO.setJoiningDate(createGravitateUserForm.joinedOn());
+        userVO.setPhoneNumber(createGravitateUserForm.phoneNumber());
+        userVO.setProfilePicturePath(createGravitateUserForm.profilePicture());
+        userVO.setContractPath(createGravitateUserForm.contractPath());
+        userVO.setEmploymentStatus(createGravitateUserForm.employmentStatus());
+        userVO.setBankName(createGravitateUserForm.bankName());
+        userVO.setAccountNumber(createGravitateUserForm.accountNumber());
+        userVO.setRoleId(createGravitateUserForm.roleId());
+        userVO.setBilling(createGravitateUserForm.billing());
+        //create user manager object
+        userManagerVO.setManagerId(createGravitateUserForm.managerId());
+        APIResponse apiResponse = gravitateUserManagerService.createGravitateUser(userVO,userManagerVO);
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .statusCode(apiResponse.getStatusCode())
+                        .data(apiResponse.getData())
+                        .build()
+        );
+    }
+    @GetMapping(value = "/")
+    public ResponseEntity<APIResponse> getAllUsersByManagerId(@RequestParam("managerId") Long managerId){
+        APIResponse apiResponse = gravitateUserManagerService.getAllGravitateUsersByManagerId(managerId);
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .statusCode(apiResponse.getStatusCode())
+                        .data(apiResponse.getData())
+                        .build()
+        );
+    }
+
+    @GetMapping(value = "/profile")
+    public ResponseEntity<APIResponse> getGravitateUsernameProfile(HttpServletRequest request){
+        String token = request.getHeader(AUTHORIZATION).substring("Bearer ".length());
+        String username = JwtUtils.getUserNameFromJwtToken(token);
+        APIResponse apiResponse = gravitateUserManagerService.getGravitateUserByUsername(username);
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .statusCode(apiResponse.getStatusCode())
+                        .data(apiResponse.getData())
+                        .build()
+        );
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<APIResponse> updateGravitateUser(){
+        return null;
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<APIResponse> deleteGravitateUser(Long userId){
+        APIResponse apiResponse = gravitateUserManagerService.deleteGravitateUser(userId);
+        return ResponseEntity.ok(
+                APIResponse.builder()
+                        .statusCode(apiResponse.getStatusCode())
+                        .data(apiResponse.getData())
+                        .build()
+        );
+    }
+
+
 }
