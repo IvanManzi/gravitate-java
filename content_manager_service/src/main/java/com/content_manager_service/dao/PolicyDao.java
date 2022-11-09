7076aa0de6530fd16db82700fb0c4f810ac3a8be
ivanmanzi5 <ivanlae@outlook.com>
2022-11-09 12:30:58 +0200

    added daos and their query implementations

--- content_manager_service/src/main/java/com/content_manager_service/dao/PolicyDao.java
+++ content_manager_service/src/main/java/com/content_manager_service/dao/PolicyDao.java
@@ -1,7 +1,15 @@
 package com.content_manager_service.dao;
 
+import com.model.PolicyVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface PolicyDao {
+
+    int createGravitatePolicy(PolicyVO policyVO);
+    List<PolicyVO> getAllPolicies(Long adminId);
+    int updateGravitatePolicy(PolicyVO policyVO);
+    int deleteGravitatePolicy(Long policyId);
 }
--- content_manager_service/src/main/java/com/content_manager_service/dao/WishDao.java
+++ content_manager_service/src/main/java/com/content_manager_service/dao/WishDao.java
@@ -1,7 +1,17 @@
 package com.content_manager_service.dao;
 
+import com.model.WishVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface WishDao {
+
+    int createWish(WishVO wishVO);
+    List<Map> getAllWishes(Long adminId);
+    int updateWish(WishVO wishVO);
+    int deleteWish(Long wishId);
+
 }
