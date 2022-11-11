a9208785d5a6182059581d4acd54e32237822562
ivanmanzi5 <ivanlae@outlook.com>
2022-11-11 15:25:19 +0200

    added missing request attribute

--- content_manager_service/src/main/java/com/content_manager_service/form/CreatePolicyRequest.java
+++ content_manager_service/src/main/java/com/content_manager_service/form/CreatePolicyRequest.java
@@ -2,5 +2,5 @@ package com.content_manager_service.form;
 
 import javax.validation.constraints.NotBlank;
 
-public record CreatePolicyRequest(@NotBlank String policyType,@NotBlank String policyName,@NotBlank String filePath) {
+public record CreatePolicyRequest(@NotBlank String policyType,@NotBlank String policyName,@NotBlank String filePath,@NotBlank String description) {
 }
