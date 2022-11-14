1a9b50d3752bd22dfd0918333a5b63d69af53dde
ivanmanzi5 <ivanlae@outlook.com>
2022-11-14 08:51:42 +0200

    Added compatible annotation

--- content_manager_service/src/main/java/com/content_manager_service/form/CreateRolePerformanceRequest.java
+++ content_manager_service/src/main/java/com/content_manager_service/form/CreateRolePerformanceRequest.java
@@ -1,6 +1,7 @@
 package com.content_manager_service.form;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-public record CreateRolePerformanceRequest(@NotBlank Long roleId,@NotBlank String criteria,@NotBlank String description) {
+public record CreateRolePerformanceRequest(@NotNull Long roleId, @NotBlank String criteria, @NotBlank String description) {
 }
