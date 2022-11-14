ea7c33f4b8c7ae563a30616dac5af08230401d1a
ivanmanzi5 <ivanlae@outlook.com>
2022-11-14 08:51:33 +0200

    Added compatible annotation

--- content_manager_service/src/main/java/com/content_manager_service/form/CreateWishRequest.java
+++ content_manager_service/src/main/java/com/content_manager_service/form/CreateWishRequest.java
@@ -1,6 +1,7 @@
 package com.content_manager_service.form;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-public record CreateWishRequest(@NotBlank Long userId,@NotBlank String wishType,@NotBlank String comment) {
+public record CreateWishRequest(@NotNull Long userId, @NotBlank String wishType, @NotBlank String comment) {
 }
