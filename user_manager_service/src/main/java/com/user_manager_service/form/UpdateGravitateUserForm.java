ea1e3741e4fb96a52797aa9c3865f08fcf3abf35
ivanmanzi5 <ivanlae@outlook.com>
2022-11-23 18:14:47 +0200

    added request attributes

--- user_manager_service/src/main/java/com/user_manager_service/form/UpdateGravitateUserForm.java
+++ user_manager_service/src/main/java/com/user_manager_service/form/UpdateGravitateUserForm.java
@@ -1,4 +1,44 @@
 package com.user_manager_service.form;
 
-public record UpdateGravitateUserForm() {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+public record UpdateGravitateUserForm(@NotNull
+                                      Long userId,
+                                      @NotBlank String userType,
+                                      @NotBlank
+                                      Integer userLevel,
+                                      @NotBlank
+                                      String firstName,
+                                      @NotBlank
+                                      String lastName,
+                                      @NotBlank
+                                      String password,
+                                      @NotBlank
+                                      String country,
+                                      @NotBlank
+                                      Date dob,
+                                      @NotBlank
+                                      Date joinedOn,
+                                      @NotBlank
+                                      String employmentStatus,
+                                      @NotBlank
+                                      String billing,
+                                      @NotBlank
+                                      String phoneNumber,
+                                      @NotBlank
+                                      String email,
+                                      @NotBlank
+                                      String alternativeEmail,
+                                      Long roleId,
+                                      @NotBlank
+                                      String bankName,
+                                      @NotBlank
+                                      String accountNumber,
+                                      @NotBlank
+                                      String profilePicture,
+                                      @NotBlank
+                                      String contractPath,
+                                      Long managerId) {
 }
