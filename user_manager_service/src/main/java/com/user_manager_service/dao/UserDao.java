3358412acd37f094e62eb77914d0d760f150d6be
ivanmanzi5 <ivanlae@outlook.com>
2022-11-23 18:14:30 +0200

    added new dao methods

--- user_manager_service/src/main/java/com/user_manager_service/dao/UserDao.java
+++ user_manager_service/src/main/java/com/user_manager_service/dao/UserDao.java
@@ -10,9 +10,14 @@ import java.util.Map;
 @Mapper
 public interface UserDao {
     int createGravitateUser(UserVO userVO);
+
+    int updateGravitateUser(UserVO userVO);
     int checkIfUsernameExists(String username);
     UserVO getGravitateUserByUsername(String username);
     int deleteGravitateUser(Long userId);
     int updateGravitateUserPassword(UserVO userVO);
-    List<Map> getAllGravitateUsers(@Param("search") String search, @Param("roleId") Long roleId );
+    List<UserVO> getAllGravitateUsers(@Param("search") String search, @Param("roleId") Long roleId );
+    List<Map> getGravitateUserTeamMembers();
+    List<UserVO> getGravitateManagerUsers(String search);
+    int updateUserStatus(Long userId);
 }
