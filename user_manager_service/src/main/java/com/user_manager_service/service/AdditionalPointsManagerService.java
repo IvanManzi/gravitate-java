56b10c1d479ebbbeee1d2b82d08129fbd27bf12a
ivanmanzi5 <ivanlae@outlook.com>
2022-12-02 16:13:37 +0200

    added new interface parameters

--- user_manager_service/src/main/java/com/user_manager_service/service/AdditionalPointsManagerService.java
+++ user_manager_service/src/main/java/com/user_manager_service/service/AdditionalPointsManagerService.java
@@ -4,8 +4,10 @@ package com.user_manager_service.service;
 import com.model.AdditionalPointVO;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Date;
+
 public interface AdditionalPointsManagerService {
     ResponseEntity createAdditionalPoint(AdditionalPointVO additionalPointVO);
 
-    ResponseEntity getUserAdditionalPoints(Long userId);
+    ResponseEntity getUserAdditionalPoints(Long userId, String quarter, Date date);
 }
