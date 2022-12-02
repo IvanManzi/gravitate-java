e27c9a864519507c6611fe14611a6f1258d3c8f5
ivanmanzi5 <ivanlae@outlook.com>
2022-12-02 08:07:19 +0200

    removed project description

--- content_manager_service/src/main/java/com/content_manager_service/form/UpdatePolicyRequest.java
+++ content_manager_service/src/main/java/com/content_manager_service/form/UpdatePolicyRequest.java
@@ -3,5 +3,5 @@ package com.content_manager_service.form;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-public record UpdatePolicyRequest(@NotNull Long policyId, @NotBlank String policyType,@NotBlank String description, @NotBlank String policyName, @NotBlank String filePath) {
+public record UpdatePolicyRequest(@NotNull Long policyId, @NotBlank String policyType,@NotBlank String policyName, @NotBlank String filePath) {
 }
