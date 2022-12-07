1e2634ebabe063c1c5a84a953016353f62e4a1b7
ivanmanzi5 <ivanlae@outlook.com>
2022-12-07 15:13:52 +0200

    added security to new module

---
+++ project_manager_service/src/main/java/com/project_manager_service/security/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.project_manager_service.security;
+
+
+import com.project_manager_service.security.filter.CustomAuthorizationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.util.Constants.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true)
+public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public CustomAuthorizationFilter authenticationJwtTokenFilter() {
+        return new CustomAuthorizationFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().antMatchers("/api/v1/project/**").hasAnyAuthority(DEVELOPER_USER,ADMIN_USER,PROJECT_LEAD);
+        http.authorizeRequests().antMatchers("/api/v1/project/**").hasAuthority(ADMIN_USER);
+        http.authorizeRequests().anyRequest().authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
