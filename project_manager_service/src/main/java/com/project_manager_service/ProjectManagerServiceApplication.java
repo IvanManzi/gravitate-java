65f1d49edd1781450aeecb35e4164735c5b06e34
ivanmanzi5 <ivanlae@outlook.com>
2022-12-07 15:15:59 +0200

    added main class for new module

---
+++ project_manager_service/src/main/java/com/project_manager_service/ProjectManagerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.project_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerServiceApplication.class,args);
+    }
+}
