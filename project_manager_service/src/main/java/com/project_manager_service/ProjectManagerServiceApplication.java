f1fa290275df85605b709d87bf2aa70b12423cad
ivan M <ivanlae@outlook.com>
2022-12-13 09:34:31 +0200

    added annotation to enable service to be discovarable

--- project_manager_service/src/main/java/com/project_manager_service/ProjectManagerServiceApplication.java
+++ project_manager_service/src/main/java/com/project_manager_service/ProjectManagerServiceApplication.java
@@ -2,8 +2,10 @@ package com.project_manager_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProjectManagerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagerServiceApplication.class,args);
