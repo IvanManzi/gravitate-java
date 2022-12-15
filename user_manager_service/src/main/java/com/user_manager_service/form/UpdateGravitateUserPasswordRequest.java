5e29895bebb63ca471192edaa4be6a7739cf9ef3
ivanmanzi5 <ivanlae@outlook.com>
2022-12-15 16:35:21 +0200

    added old password in update password request

--- user_manager_service/src/main/java/com/user_manager_service/form/UpdateGravitateUserPasswordRequest.java
+++ user_manager_service/src/main/java/com/user_manager_service/form/UpdateGravitateUserPasswordRequest.java
@@ -1,6 +1,11 @@
 package com.user_manager_service.form;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-public record UpdateGravitateUserPasswordRequest(@NotBlank String password) {
+public record UpdateGravitateUserPasswordRequest(@NotBlank
+                                                 String password,
+                                                 @NotNull
+                                                 String oldPassword
+                                                 ) {
 }
