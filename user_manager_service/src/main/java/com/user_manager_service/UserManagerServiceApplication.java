53f6e98e1d05cbf48b0443c14b5849a48e9492ab
ivan M <ivanlae@outlook.com>
2023-01-21 21:18:50 +0200

    added @EnableScheduling to support for scheduling tasks

--- user_manager_service/src/main/java/com/user_manager_service/UserManagerServiceApplication.java
+++ user_manager_service/src/main/java/com/user_manager_service/UserManagerServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableScheduling
 public class UserManagerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserManagerServiceApplication.class,args);
