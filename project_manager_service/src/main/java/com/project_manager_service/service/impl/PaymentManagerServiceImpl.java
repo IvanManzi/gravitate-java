e26042d501ad7f038bd28af3d05144e068e51fb5
ivan M <ivanlae@outlook.com>
2023-02-01 15:49:41 +0200

    fix: prevent marking tasks and incentives as completed before the end of the month

--- project_manager_service/src/main/java/com/project_manager_service/service/impl/PaymentManagerServiceImpl.java
+++ project_manager_service/src/main/java/com/project_manager_service/service/impl/PaymentManagerServiceImpl.java
@@ -6,9 +6,14 @@ import com.project_manager_service.dao.ProjectIncentiveDao;
 import com.project_manager_service.dao.TaskReportDao;
 import com.project_manager_service.service.PaymentManagerService;
 import com.util.APIResponse;
+import com.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +21,29 @@ public class PaymentManagerServiceImpl implements PaymentManagerService {
 
     private final TaskReportDao taskReportDao;
 
-
     private final PaymentDao paymentDao;
 
     private final ProjectIncentiveDao projectIncentiveDao;
 
     @Override
     public ResponseEntity<APIResponse> markMonthlyTasksAndIncentivesAsPaid(PaymentVO paymentVO) {
+        Calendar ca =  Calendar.getInstance();
+        int currentMonth = ca.get(Calendar.MONTH);
+        currentMonth++;
+        if(currentMonth == paymentVO.getMonth()){
+            if(DateUtil.getCurrentDayEndTime().compareTo(DateUtil.getCurrentMonthEndTime()) == 0){
+                int result = taskReportDao.markMonthlyTasksAsPaid(paymentVO.getUserId(),paymentVO.getMonth(), paymentVO.getYear(), paymentVO.getProjectId());
+                //mark the current month incentives as paid
+                int result2 = projectIncentiveDao.markMonthlyProjectIncentiveAsPaid(paymentVO.getMonth(),paymentVO.getYear());
+                //create payment record
+                int result3 = paymentDao.createPaymentRecord(paymentVO);
+                if((result > 0) && (result2 > 0) && (result3 > 0) ){
+                    return APIResponse.resultSuccess("User task reports marked as paid.");
+                }
+                return APIResponse.resultFail();
+            }
+            return APIResponse.resultFail("You can't mark tasks as paid until the end of month. ");
+        }
         int result = taskReportDao.markMonthlyTasksAsPaid(paymentVO.getUserId(),paymentVO.getMonth(), paymentVO.getYear(), paymentVO.getProjectId());
         //mark the current month incentives as paid
         int result2 = projectIncentiveDao.markMonthlyProjectIncentiveAsPaid(paymentVO.getMonth(),paymentVO.getYear());
