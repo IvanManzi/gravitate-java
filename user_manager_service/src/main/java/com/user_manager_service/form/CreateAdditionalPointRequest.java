79bdab2ef6c5a3f05e450c378ea72d937655228f
ivan M <ivanlae@outlook.com>
2023-02-02 12:29:07 +0200

    fix: use suitable validator

--- user_manager_service/src/main/java/com/user_manager_service/form/CreateAdditionalPointRequest.java
+++ user_manager_service/src/main/java/com/user_manager_service/form/CreateAdditionalPointRequest.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public record CreateAdditionalPointRequest(@NotNull Long userId,
-                                           @NotBlank Integer quarter,
+                                           @NotNull Integer quarter,
                                            @NotNull Integer points,
                                            @NotBlank String comment,
                                            @NotBlank String category) {
