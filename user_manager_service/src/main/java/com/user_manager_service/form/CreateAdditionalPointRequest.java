b62cab8e30eb28084e30eb53ffb7c3e4f374f17f
ivan M <ivanlae@outlook.com>
2023-02-02 21:10:58 +0200

    added new request attributes

--- user_manager_service/src/main/java/com/user_manager_service/form/CreateAdditionalPointRequest.java
+++ user_manager_service/src/main/java/com/user_manager_service/form/CreateAdditionalPointRequest.java
@@ -2,9 +2,13 @@ package com.user_manager_service.form;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 public record CreateAdditionalPointRequest(@NotNull Long userId,
                                            @NotNull Integer quarter,
+                                           Long userSkillId,
+                                           List<Long> blogIds,
+                                           List<Long> forumAnswerIds,
                                            @NotNull Integer points,
                                            @NotBlank String comment,
                                            @NotBlank String category) {
