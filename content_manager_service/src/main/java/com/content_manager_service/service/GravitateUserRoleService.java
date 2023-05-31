3d730d9af8ded223e32d08ccfaad4b0cda7369d3
IvanManzi <108268160+IvanManzi@users.noreply.github.com>
2023-05-31 16:24:47 +0200

    Update GravitateUserRoleService.java

--- content_manager_service/src/main/java/com/content_manager_service/service/GravitateUserRoleService.java
+++ content_manager_service/src/main/java/com/content_manager_service/service/GravitateUserRoleService.java
@@ -1,15 +1,15 @@
 package com.content_manager_service.service;
 
 import com.model.RoleVO;
-import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity;
 
 public interface GravitateUserRoleService {
 
-    ResponseEntity createUserRole(RoleVO roleVO)
+    ResponseEntity createUserRole(RoleVO roleVO);
 
-    ResponseEntity getAllUserRoles(Long userId)
+    ResponseEntity getAllUserRoles(Long userId);
 
-    ResponseEntity updateUserRole(RoleVO roleVO)
+    ResponseEntity updateUserRole(RoleVO roleVO);
 
-    ResponseEntity deleteUserRole(Long roleId)
+    ResponseEntity deleteUserRole(Long roleId);
 }
